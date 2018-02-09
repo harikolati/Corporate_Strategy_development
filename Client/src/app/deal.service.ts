d988a26a0e79c60b6b22f1a684bec914de1de914
harikolati <36236524+harikolati@users.noreply.github.com>
2018-02-09 18:09:18 +0530

    Update deal.service.ts

--- Client/src/app/deal.service.ts
+++ Client/src/app/deal.service.ts
@@ -1,26 +1,34 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Deal } from './deal';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
-
+import { Deal } from './Deal';
+import { Http, Response, Headers } from "@angular/http";
+import { Injectable} from "@angular/core";
+import 'rxjs/Rx';
+import { Observable } from "rxjs";
 @Injectable()
-export class DealService {
-
-  private _url: string = "/assets/data/activeDealInfo.json"; 
-  
-    constructor(private http:HttpClient) { }
-
-    getDealDetails(): Observable<Deal[]>{
-  
-      return this.http.get<Deal[]>(this._url)  
-                      .catch(this.errorHandler);
-  
+export class DealService{
+constructor(private http: Http) {}
+    private deals : Deal[]=[];
+    getDealDetails(){
+    return this.http.get('http://localhost:3000/deals')
+            .map((response: Response) => {
+                const deals = response.json().obj;
+                let transformedDeals: Deal[] = [];
+                for (let deal of deals) {
+                    console.log(deal);
+                    transformedDeals.push(new Deal(
+                     deal.transactionType,
+                     deal.transactionStage,
+                     deal.dealCode,
+                     deal.dealType,
+                     deal.marketSegment,
+                     deal.targetMarketSegment,
+                     deal.targetCompanyName,
+                     deal.purchasePrice));
+                }
+                this.deals = transformedDeals;
+                return transformedDeals;
+            })
+            .catch((error: Response) => {console.log(error)
+             console.log(error);
+                          return Observable.throw(error);});
     }
-    errorHandler(error: HttpErrorResponse){  
-      return Observable.throw(error.message || "Server Error");  
-    }
-  
-  
-  }
+}
