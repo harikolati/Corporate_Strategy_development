2834784a253077eed9766aabe9e92c049f61582b
harikolati <36236524+harikolati@users.noreply.github.com>
2018-02-15 13:02:52 +0530

    Update deals.js

--- server/routes/deals.js
+++ server/routes/deals.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
 var path = require('path');
 var Deal = require('../models/deal');
+var DealRole = require('../models/dealRole');
 
 router.get('/getDeals',function(req,res){
     res.sendFile(path.join(__dirname,'../public','uploadDeals.html'));
@@ -30,6 +32,27 @@ router.get('/', function (req, res, next) {
         });
 });
 
+router.get('/dealRole', function (req, res, next) {
+  var userName=req.query.userName;
+DealRole.
+  find({users : userName,
+    dealRoleName: 'Integration Lead'
+  }).
+  populate('deals').
+  exec(function(err, doc) {   
+    if (err) {
+                console.log("ERROR");
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                obj: doc
+            });
+  });
 
+});
 
 module.exports = router;
